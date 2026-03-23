5e73c73fdacd6293b72d6c43a0b4a6972c06757a
Manisha8905 <manisha_gaur@technologymindz.com>
2026-03-23 17:45:31 +0600

    font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins, Inter } from "next/font/google";
+import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import Toaster from "../app/Componets/Toaster";
 import LayoutWrapper from "./LayoutWrapper";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100","200","300","400","500","600","700","800","900"],
-});
-
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+  display: "swap",
+});
+
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-jakarta",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} ${inter.variable} antialiased`} suppressHydrationWarning>
+      <body className={`${inter.variable} ${jakarta.variable} antialiased`} suppressHydrationWarning>
         <Providers>
           <LayoutWrapper>{children}</LayoutWrapper>
           <Toaster />
