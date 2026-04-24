fe4bd477213b35c1b7f6d4b281709480d3ed0e04
AnandSwarupp <anandswarup070@gmail.com>
2026-04-24 19:46:01 +0530

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        has: [{ type: "host", value: "campaign-management-1.technologymindz.com" }],
+        destination: "https://ai-sdr-campaign-management-elevenlabs-1.technologymindz.com/:path*",
+      },
+      {
+        source: "/backend/:path*",
+        has: [{ type: "host", value: "campaign-management-2.technologymindz.com" }],
+        destination: "https://demo-api.technologymindz.net/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
